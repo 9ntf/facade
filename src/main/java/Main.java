7a319530cabda5b4c29f0681faf2a9713c299280
Alexey Khmelev <a.khmelev91@gmail.com>
2021-08-10 23:48:59 +1000

    facade

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import facade.BinOps;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("1111", "1110"));
+        System.out.println(bins.mult("10", "11"));
+    }
+}
